a2e784f4eeb9ae373c1ff614eb81a68c00ebc689
dat.le <dat.le3@ntq-solution.com.vn>
2019-10-29 14:35:37 +0700

    Undoing scenarios

--- git-training/undoing/env-init.sh
+++ git-training/undoing/env-init.sh
@@ -18,9 +18,3 @@ git commit -m "Add new file 3"
 
 git add file4.txt
 git commit -m "Add new file 4"
-
-
-echo "File 1 new content" >> file1.txt
-echo "File 2 new content" >> file2.txt
-echo "File 3 new content" >> file3.txt
-echo "File 4 new content" >> file4.txt
---
+++ git-training/undoing/step1-init.sh
@@ -0,0 +1,4 @@
+echo "File 1 new content" >> file1.txt
+echo "File 2 new content" >> file2.txt
+echo "File 3 new content" >> file3.txt
+echo "File 4 new content" >> file4.txt
