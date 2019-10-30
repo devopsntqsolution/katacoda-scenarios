783622fc88a54310051757bd0eeb2eb8009a8305
dat.le <dat.le3@ntq-solution.com.vn>
2019-10-30 10:08:10 +0700

    Updating branch

--- git-training/updating-branch/step1/init.sh
+++ git-training/updating-branch/step1/init.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 git checkout -b feature_1
-echo "Update new content file1 from feature 1" >> file1.txt
-git commit -am "Update from feature_1"
-git checkout master
+echo "[1] Update new content file1 from feature 1" >> file1.txt
+git commit -am "[1] Update from feature_1"
 
-echo "Update new content file2 from master" >> file2.txt
-git commit -am "[1] Update new content file2"
+echo "[2] Update new content file1 from feature 1" >> file1.txt
+git commit -am "[2] Update from feature_1"
+
+git checkout master
+sleep 1
+echo "[3] Update new content file2 from master" >> file2.txt
+git commit -am "[3] Update from master"
--- git-training/updating-branch/step2/answer.md
+++ git-training/updating-branch/step2/answer.md
@@ -1,8 +1,8 @@
 ### Hiển thị danh sách các nhánh và commit gần nhất
 `git branch -av`{{copy}}
 
-### Rebase nhánh feature_2 với master
-`git rebase master`
+### Rebase nhánh feature_2 với staging
+`git rebase staging`
 
 ### Hiển thị lịch sử commits
 `git log` 
