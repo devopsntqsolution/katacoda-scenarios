6d134a89af911ff06676f9f91059c998726ba8a9
dat.le <dat.le3@ntq-solution.com.vn>
2019-10-31 10:39:34 +0700

    Updating branch

--- git-training/updating-branch/step1/init.sh
+++ git-training/updating-branch/step1/init.sh
@@ -8,7 +8,7 @@ git branch feature/1
 cd ../working
 git fetch origin
 git checkout feature/1
-echo "update feature/1" | tee -a file1.txt file2.txt
+echo "[feature/1] update by you" | tee -a file1.txt file2.txt
 
 git add file1.txt
 git commit -m "[1][feature/1] You update file1.txt"
@@ -20,12 +20,12 @@ git commit -m "[2][feature/1] You update file2.txt"
 cd ../repository
 sleep 1
 git checkout feature/1
-echo "update feature/1" | tee -a file1.txt file2.txt
+echo "[feature/1] update by another" | tee -a file1.txt file2.txt
 
 git add file1.txt
 git commit -m "[3][feature/1] Another update file1.txt"
 
 git add file2.txt
-git commit -m "[3][feature/1] Another update file2.txt"
+git commit -m "[4][feature/1] Another update file2.txt"
 
 cd ..
