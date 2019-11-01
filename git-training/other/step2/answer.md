7b1efce4999f271e718d75529b2c62a6ce402d73
dat.le <dat.le3@ntq-solution.com.vn>
2019-11-01 11:36:33 +0700

    Other scenarios

--- git-training/other/step2/answer.md
+++ git-training/other/step2/answer.md
@@ -3,4 +3,8 @@
 
 ### Khôi phục trạng thái
 `git reset --hard HEAD@{n}`
+
+### Lấy lại branch bị xóa
+`git checkout -b <branch_name> HEAD@{n}`
+
 n: trạng thái muốn khôi phục trong lịch sử thay đổi hiển thị bởi lệnh `git reflog`
--- git-training/other/step2/init.sh
+++ git-training/other/step2/init.sh
@@ -3,12 +3,16 @@
 # create feature/1 branch
 cd repository
 git checkout master
-git branch feature/1
 git branch feature/2
+git checkout -b feature/3
+echo "[feature/3] update file" >> file1.txt
+git add file1.txt
+git commit -m "[feature/3] update file1.txt"
+
 git push --all origin
 
 cd ../working
 
 git fetch origin
-git checkout feature/1
 git checkout feature/2
+git checkout feature/3
